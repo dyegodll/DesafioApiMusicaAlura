9ced7a1c6ab0afe012c72ceffbc1bec5a6bf150a
Dyego Lima <dyegodll18@gmail.com>
2024-12-12 09:06:58 -0300

    first commit

---
+++ DesafioApiMusicaAlura/Model/Musica.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DesafioApiMusicaAlura.Model;
+
+internal class Musica
+{
+    [JsonPropertyName("artist")]
+    public string Artist { get; set; }
+    [JsonPropertyName("song")]
+    public string Song { get; set; }
+    [JsonPropertyName("year")]
+    public string Year { get; set; }
+    [JsonPropertyName("key")]
+    public int Key { get; set; }
+    [JsonPropertyName("genre")]
+    public string Genre { get; set; }
+
+    string[] tonalidade = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+
+    public void TomDaMusica(int Key)
+    {
+        Console.WriteLine($"Artista: {Artist} - Música: {Song} - Ano: {Year} - Gênero: {Genre} - Tonalidade: {tonalidade[Key]}\n");
+    }
+
+}
---
+++ DesafioApiMusicaAlura/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioApiMusicaAlura.Model;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    string json = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    var musicaList = JsonSerializer.Deserialize<List<Musica>>(json)!;
+
+    foreach (var musica in musicaList)
+    {
+        musica.TomDaMusica(musica.Key);
+    }
+}
